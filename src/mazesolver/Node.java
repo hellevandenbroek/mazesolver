76b9ad865c31472fdd0ac9d6ae4d3518fdd07275
Helle van den Broek <hvbroek@stud.ntnu.no>
2018-06-27 13:37:08 +0200

    first

---
+++ src/mazesolver/Node.java
@@ -0,0 +1,18 @@
+package mazesolver;
+
+public class Node {
+
+	//kan ha venstre, høyre, opp, ned
+	//oppdaget, uoppdaget farge? boolean?
+	//hvor kommer vi fra
+	
+	//konstruktør
+	
+	public Node() {
+		//initialisering
+	}
+
+	
+	
+}
+
---
+++ src/mazesolver/Solve.java
@@ -0,0 +1,9 @@
+package mazesolver;
+
+public class Solve {
+	//trenger et startpunkt
+	//trenger et sluttpunkt
+	//trenger størrelse på maze, høyde, bredde
+	//piksler: hvite eller svarte
+	//hvite: kan gå dit. svarte: kan ikke gå dit
+}
